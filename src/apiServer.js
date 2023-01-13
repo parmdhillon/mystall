366fe204128fa078a180d43b689dfd123616f4e8
Parminder Singh <46890264+parmdhillon@users.noreply.github.com>
2023-01-13 13:50:41 -0500

    Update apiServer.js

--- src/apiServer.js
+++ src/apiServer.js
@@ -1,5 +1,5 @@
 const API_SERVER =
   process.env.NODE_ENV === 'development'
     ? ''
-    : 'https://mystall.herokuapp.com';
+    : 'https://discovered-amenable-island.glitch.me';
 export default API_SERVER;
